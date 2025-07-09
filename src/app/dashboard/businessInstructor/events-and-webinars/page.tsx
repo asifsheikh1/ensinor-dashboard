2b102e9c6dfa2f570ed821a400d4fa3bc03c5967
asifsheikh1 <nmasifsheikh2003@gmail.com>
2025-07-09 13:05:50 +0600

    feat: Create CreateEventAndSchedule component and integrate it into EventsAndWebinars page

--- src/app/dashboard/businessInstructor/events-and-webinars/page.tsx
+++ src/app/dashboard/businessInstructor/events-and-webinars/page.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import SectionHeader from "@/components/SectionHeader";
+import React from "react";
 
 export default function EventsAndWebinars() {
-  return (
-    <div>EventsAndWebinars</div>
-  )
+  return <div className="p-6 space-y-6">
+    {/* Section Header */}
+    <SectionHeader title="Events & Webinars" description="Schedule and manage your live events" leftContent="null" options={{name: 'link', content: 'Schedule Event', link: '/dashboard/events-and-webinars/create-event-and-schedule'}} />
+
+    {/* Event and Webinars Cards */}
+    
+  </div>;
 }
